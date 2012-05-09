98e0c44743beec02ecd7dd50e72b295ad877dbb6
Eoghan O'Brien <eoghan@eoghanobrien.com>
2012-05-09 20:53:38 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-suggestfield
+# suggestfield
 ============
 
 ExpressionEngine fieldtype, almost identical to the native text field with the added benefit that it will search previous values and autosuggest them to the editor.
---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+if (! defined('SUGGESTFIELD_NAME'))
+{
+	define('SUGGESTFIELD_NAME', 'SuggestField');
+	define('SUGGESTFIELD_VERSION',  '1.0');
+	define('SUGGESTFIELD_SHORTNAME', 'suggestfield');
+}
+
+$config['name']    = SUGGESTFIELD_NAME;
+$config['version'] = SUGGESTFIELD_VERSION;
